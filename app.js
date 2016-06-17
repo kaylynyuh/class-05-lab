d0da377784cdc996d074e20f866d4c8535f147df
kaylynyuh <yuhkay4@gmail.com>
2016-06-17 15:38:34 -0700

    question 3 complete

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ Test this function by hand in the console to get it working, and when you think 
 function multiply(a,b){
   var result = a * b;
   var message = 'The product of ' + a + ' and ' + b + ' is ' + result + '.';
-
   return [result, message];
 }
 
@@ -49,11 +48,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){
-
+  var firstSum = sum(a,b)[0];
+  var secondSum = sum(firstSum,c)[0];
+  var firstMultiply = multiply(a,b)[0];
+  var secondMultiply = multiply(firstMultiply,c)[0];
+  var message1 = a + ' and ' + b + ' and ' + c + ' sum to ' + secondSum + '.';
+  var message2 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + secondMultiply + '.';
+  return [secondSum, secondMultiply, message1, message2];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. don't forget to create a new branch for your work on the next question!
 
@@ -123,7 +128,7 @@ function testMultiply() {
 }
 
 function testSumAndMultiply() {
-  if (sumAndMultiply(4, 7, 5)[1] === '4 and 7 and 5 sum to 16.' && sumAndMultiply(4, 7, 5)[2] === 'The product of 4 and 7 and 5 is 140.') {
+  if (sumAndMultiply(4, 7, 5)[2] === '4 and 7 and 5 sum to 16.' && sumAndMultiply(4, 7, 5)[3] === 'The product of 4 and 7 and 5 is 140.') {
     console.log('%c TEST FOR sumAndMultiply() PASSES', 'color: green');
   } else {
     console.log('%c TEST FOR sumAndMultiply() FAILS', 'color: red');
